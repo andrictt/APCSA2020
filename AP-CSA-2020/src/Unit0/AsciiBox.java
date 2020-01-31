98735ae5890fbe3c747024c433f2461eab32a08e
lia7952 <lia7952@CA-SU-F106-07.sduhsd.lan>
2020-01-31 10:35:02 -0800

    Finished Labs in Class

---
+++ AP-CSA-2020/src/Unit0/AsciiBox.java
@@ -0,0 +1,43 @@
+package Unit0;
+
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class AsciiBox {
+	public static void main(String[] args) {
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 3; j++) {
+				for (int k = 0; k < 14; k++) {
+					System.out.print(Plus());
+				}
+				System.out.println();
+			}
+			for (int j = 0; j < 2; j++) {
+				try {
+					for (int k = 0; k < 14; k++) {
+						if (i >= 3) {
+							throw new Exception();
+						}
+						System.out.print(A());
+					}
+					System.out.println();
+				} catch (Exception e) {
+					// e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	public static String Plus() {
+		return "+";
+	}
+
+	public static String A() {
+		return "A";
+	}
+}
---
+++ AP-CSA-2020/src/Unit0/Variables.java
@@ -0,0 +1,43 @@
+package Unit0;
+
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class Variables
+{
+	public static void main(String[] args) {
+	    byte byteOne = 127;
+	    short shortOne = -32123;
+	    int intOne = 90877;
+	    long longOne = 999999999;
+
+	    float floatOne = 38.5678f;
+	    double doubleOne = 923.234567890134823546782453678243567892435467894365789;
+
+	    char charOne = 'A';
+
+	    boolean booleanOne = true;
+	    String stringOne = "hello world";
+
+	    System.out.println("/////////////////////////////////");
+	    System.out.println("Andric Li 01/30/20");
+	    System.out.println("integer types  \n");
+	    System.out.println("8 bit - byteOne = " + byteOne);
+	    System.out.println("16 bit - shortOne = " + shortOne);
+	    System.out.println("32 bit - intOne = " + intOne);
+	    System.out.println("64 bit - longOne = " + longOne);
+	    System.out.println("real types  \n");
+	    System.out.println("32 bit - floatOne = " + floatOne);
+	    System.out.println("64 bit - doubleOne = " + doubleOne);
+	    System.out.println("other integer types  \n");
+	    System.out.println("16 bit - charOne = " + charOne);
+	    System.out.println("other types  \n");
+	    System.out.println("booleanOne = " + booleanOne);
+	    System.out.println("StringOne = " + stringOne);
+	  }
+}
