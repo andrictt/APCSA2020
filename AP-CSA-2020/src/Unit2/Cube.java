02c5082359e864ee6296635d999f66dc75e07bbc
andrictt <andrictt@gmail.com>
2020-02-02 21:23:24 -0800

    Finished Unit 2

---
+++ AP-CSA-2020/src/Unit2/Cube.java
@@ -0,0 +1,14 @@
+package Unit2;
+
+//(c) A+ Computer Science
+// www.apluscompsci.com
+//Name -
+//Date -
+
+public class Cube
+{
+	public static double area( int side )
+	{
+		return Math.pow(side, 2)*6;
+	}
+}
---
+++ AP-CSA-2020/src/Unit2/Line.java
@@ -0,0 +1,15 @@
+package Unit2;
+
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+//Date -
+
+public class Line
+{
+	public static double getSlope( int x1, int y1, int x2, int y2 )
+	{
+		return (double)(y2-y1)/(double)(x2-x1);
+	}
+
+}
