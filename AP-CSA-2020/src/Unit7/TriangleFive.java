4ced9adb59c30678691b2dafd1f89b5b089aa6d0
lia7952 <lia7952@CA-SU-F106-07.sduhsd.lan>
2020-02-21 09:56:59 -0800

    Finished Unit 7

--- AP-CSA-2020/src/Unit7/TriangleFive.java
+++ AP-CSA-2020/src/Unit7/TriangleFive.java
@@ -12,22 +12,41 @@ public class TriangleFive
 
 	public TriangleFive()
 	{
+		letter = 'C';
+		amount = 1;
 	}
 
 	public TriangleFive(char c, int amt)
 	{
+		letter = c;
+		amount = amt;
 	}
 
 	public void setLetter(char c)
 	{
+		letter = c;
 	}
 
 	public void setAmount(int amt)
 	{
+		amount = amt;
 	}
 
 	public String toString()
 	{
+		for(int h = 0; h < amount; h++) {
+			for(int i = 0; i < amount - h; i++) {
+				for (int j = i; j < amount; j++) {
+					char heyy = (char) (letter + i);
+					if (heyy > 90) {
+						heyy = (char)(heyy - 26);
+					}
+					System.out.print(heyy);
+				}
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
 		String output="";
 		return output;
 	}
--- AP-CSA-2020/src/Unit7/TriangleFiveRunner.java
+++ AP-CSA-2020/src/Unit7/TriangleFiveRunner.java
@@ -9,5 +9,19 @@ public class TriangleFiveRunner
 {
    public static void main(String args[])
    {
-	}
+	   TriangleFive hey = new TriangleFive('C', 4);
+	   System.out.println(hey);
+	   
+	   hey = new TriangleFive('A', 5);
+	   System.out.println(hey);
+	   
+	   hey = new TriangleFive('B', 7);
+	   System.out.println(hey);
+	   
+	   hey = new TriangleFive('X', 6);
+	   System.out.println(hey);
+	   
+	   hey = new TriangleFive('Z', 8);
+	   System.out.println(hey);
+   }
 }
